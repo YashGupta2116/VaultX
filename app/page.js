f705a7ce0c5652cee49168a81f623e318ba48e91
Yash Gupta <yashgupta2116@gmail.com>
2025-04-06 13:48:59 +0530

    made 50% of the hero section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import {Button} from '@/components/ui/button';
-import Image from 'next/image';
+import HeroSection from '@/components/hero';
 
 export default function Home() {
   return (
     <div className='mt-40'>
-      <Button variant='destructive'>Subscribe to my diq</Button>
+      <HeroSection />
     </div>
   );
 }
