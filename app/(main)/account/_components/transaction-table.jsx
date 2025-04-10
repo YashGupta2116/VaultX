c6379aaaefdc05ab910fa82e2bd312978674eeb9
Yash Gupta <yashgupta2116@gmail.com>
2025-04-10 18:09:31 +0530

    created transaction-table component

---
+++ app/(main)/account/_components/transaction-table.jsx
@@ -0,0 +1,397 @@
+'use client';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import {Checkbox} from '@/components/ui/checkbox';
+import React, {useMemo, useState} from 'react';
+import {format} from 'date-fns';
+import {categoryColors} from '@/data/categories';
+import {Badge} from '@/components/ui/badge';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import {
+  ChevronDown,
+  ChevronUp,
+  Clock,
+  MoreHorizontal,
+  RefreshCw,
+  Search,
+  Trash,
+  X,
+} from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {Button} from '@/components/ui/button';
+import {useRouter} from 'next/navigation';
+import {Input} from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+const RECURRING_INTERVALS = {
+  DAILY: 'Daily',
+  WEEKLY: 'Weekly',
+  MONTHLY: 'Monthly',
+  YEARLY: 'Yearly',
+};
+
+const TransactionTable = ({transactions}) => {
+  const router = useRouter();
+  const [selectedIds, setSelectedIds] = useState([]);
+  const [sortConfig, setSortConfig] = useState({
+    field: 'date',
+    direction: 'desc',
+  });
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [typeFilter, setTypeFilter] = useState('');
+  const [recurringFilter, setRecurringFilter] = useState('');
+
+  const filteredAndSortedTransactions = useMemo(() => {
+    let result = [...transactions];
+
+    // search filter
+    if (searchTerm) {
+      const searchLower = searchTerm.toLowerCase();
+      result = result.filter((transaction) =>
+        transaction.description?.toLowerCase().includes(searchLower)
+      );
+    }
+
+    // type filter
+    if (typeFilter) {
+      result = result.filter((transaction) => transaction.type === typeFilter);
+    }
+
+    // recurring filter
+    if (recurringFilter) {
+      result = result.filter((transaction) => {
+        if (recurringFilter === 'recurring') return transaction.isRecurring;
+        return !transaction.isRecurring;
+      });
+    }
+
+    // apply sorting
+
+    result.sort((a, b) => {
+      let comparision = 0;
+
+      switch (sortConfig.field) {
+        case 'date':
+          comparision = new Date(a.date) - new Date(b.date);
+          break;
+        case 'amount':
+          comparision = a.amount - b.amount;
+          break;
+        case 'category':
+          comparision = a.category.localeCompare(b.category);
+          break;
+
+        default:
+          comparision = 0;
+      }
+
+      return sortConfig.direction === 'asc' ? comparision : -comparision;
+    });
+
+    return result;
+  }, [transactions, searchTerm, typeFilter, recurringFilter, sortConfig]);
+
+  const handleSort = (field) => {
+    setSortConfig((current) => ({
+      field,
+      direction:
+        current.field == field && current.direction === 'asc' ? 'desc' : 'asc',
+    }));
+  };
+
+  const handleSelect = (id) => {
+    setSelectedIds((current) =>
+      current.includes(id)
+        ? current.filter((item) => item != id)
+        : [...current, id]
+    );
+  };
+
+  const handleSelectAll = () => {
+    setSelectedIds((current) =>
+      current.length === filteredAndSortedTransactions.length
+        ? []
+        : filteredAndSortedTransactions.map((t) => t.id)
+    );
+  };
+
+  const handleBulkDelete = () => {};
+
+  const handleClearFilers = () => {
+    setRecurringFilter('');
+    setSearchTerm('');
+    setTypeFilter('');
+    setSelectedIds([]);
+  };
+
+  return (
+    <div className='space-y-4'>
+      {/* Filters nd search box */}
+      <div className='flex flex-col sm:flex-row gap-4'>
+        <div className='relative flex-1'>
+          <Search className='absolute left-2 top-2.5 h-4 w-4 text-muted-foreground' />
+          <Input
+            placeholder='Search transactions...'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className={`pl-8`}
+          />
+        </div>
+
+        <div className='flex gap-2'>
+          <Select value={typeFilter} onValueChange={setTypeFilter}>
+            <SelectTrigger>
+              <SelectValue placeholder='All Types' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='INCOME'>Income</SelectItem>
+              <SelectItem value='EXPENSE'>Expense</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select
+            value={recurringFilter}
+            onValueChange={(value) => setRecurringFilter(value)}
+          >
+            <SelectTrigger className={`w-[150px]`}>
+              <SelectValue placeholder='All Transactions' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='recurring'>Recurring Only</SelectItem>
+              <SelectItem value='non-recurring'>Non-Recurring only</SelectItem>
+            </SelectContent>
+          </Select>
+
+          {selectedIds.length > 0 && (
+            <div className='flex items-center gap-2'>
+              <Button
+                variant={'destructive'}
+                size={'sm'}
+                onClick={handleBulkDelete}
+              >
+                <Trash className='h-4 w-4 mr-2' />
+                Delete Selected ({selectedIds.length})
+              </Button>
+            </div>
+          )}
+
+          {(searchTerm || typeFilter || recurringFilter) && (
+            <Button
+              variant={'outline'}
+              size={'icon'}
+              onClick={handleClearFilers}
+              title='Clear Filters'
+            >
+              <X className='h-4 w-5' />
+            </Button>
+          )}
+        </div>
+      </div>
+
+      {/* Transactions */}
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader className={`text-muted-foreground`}>
+            <TableRow>
+              <TableHead className={`w-[50px]`}>
+                <Checkbox
+                  onCheckedChange={handleSelectAll}
+                  checked={
+                    selectedIds.length ===
+                      filteredAndSortedTransactions.length &&
+                    filteredAndSortedTransactions.length > 0
+                  }
+                />
+              </TableHead>
+              <TableHead
+                className={`cursor-pointer`}
+                onClick={() => handleSort('date')}
+              >
+                <div className='flex items-center'>
+                  Date{' '}
+                  {sortConfig.field === 'date' &&
+                    (sortConfig.direction === 'asc' ? (
+                      <ChevronUp className='ml-1 h-4 w-4 ' />
+                    ) : (
+                      <ChevronDown className='ml-1 h-4 w-4' />
+                    ))}
+                </div>
+              </TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead
+                className={`cursor-pointer`}
+                onClick={() => handleSort('category')}
+              >
+                <div className='flex items-center'>
+                  Category{' '}
+                  {sortConfig.field === 'category' &&
+                    (sortConfig.direction === 'asc' ? (
+                      <ChevronUp className='ml-1 h-4 w-4 ' />
+                    ) : (
+                      <ChevronDown className='ml-1 h-4 w-4' />
+                    ))}
+                </div>
+              </TableHead>
+              <TableHead
+                className={`cursor-pointer`}
+                onClick={() => handleSort('amount')}
+              >
+                <div className='flex items-center justify-end'>
+                  Amount{' '}
+                  {sortConfig.field === 'amount' &&
+                    (sortConfig.direction === 'asc' ? (
+                      <ChevronUp className='ml-1 h-4 w-4 ' />
+                    ) : (
+                      <ChevronDown className='ml-1 h-4 w-4' />
+                    ))}
+                </div>
+              </TableHead>
+              <TableHead>Recurring</TableHead>
+              <TableHead className={`w-[50px]`}></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {filteredAndSortedTransactions.length === 0 ? (
+              <TableRow>
+                <TableCell
+                  colSpan={7}
+                  className={`text-center text-muted-foreground`}
+                >
+                  No Transaction Found
+                </TableCell>
+              </TableRow>
+            ) : (
+              filteredAndSortedTransactions.map((transaction) => (
+                <TableRow key={transaction.id}>
+                  <TableCell>
+                    <Checkbox
+                      onCheckedChange={() => handleSelect(transaction.id)}
+                      checked={selectedIds.includes(transaction.id)}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    {format(new Date(transaction.date), 'PP')}
+                  </TableCell>
+                  <TableCell>{transaction.description}</TableCell>
+                  <TableCell className={`capitalize`}>
+                    <span
+                      style={{
+                        background: categoryColors[transaction.category],
+                      }}
+                      className='px-2 py-1 rounded text-white text-sm'
+                    >
+                      {transaction.category}
+                    </span>
+                  </TableCell>
+                  <TableCell
+                    style={{
+                      color: transaction.type === 'EXPENSE' ? 'red' : 'green',
+                    }}
+                    className='text-right font-medium'
+                  >
+                    {transaction.type === 'EXPENSE' ? '-' : '+'}$
+                    {transaction.amount.toFixed(2)}
+                  </TableCell>
+                  <TableCell>
+                    {transaction.isRecurring ? (
+                      <TooltipProvider delayDuration={600}>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <Badge
+                              variant={'outline'}
+                              className={`gap-1 bg-purple-100 text-purple-700 hover:bg-purple-200`}
+                            >
+                              <RefreshCw className='h-3 w-3' />
+                              {
+                                RECURRING_INTERVALS[
+                                  transaction.recurringInterval
+                                ]
+                              }
+                            </Badge>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <div className='text-sm'>
+                              <div className='font-medium'>Next Date:</div>
+                              <div>
+                                {format(
+                                  new Date(transaction.nextRecurringDate),
+                                  'PP'
+                                )}
+                              </div>
+                            </div>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    ) : (
+                      <Badge variant={'outline'} className={`gap-1`}>
+                        <Clock className='h-3 w-3' />
+                        One-Time
+                      </Badge>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <Button
+                          asChild
+                          variant={'ghost'}
+                          className={`h-8 w-8 p-0`}
+                        >
+                          <MoreHorizontal className='h-4 w-4' />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            router.push(
+                              `/transaction/create?edit=${transaction.id}`
+                            );
+                          }}
+                        >
+                          Edit
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem
+                          className={`text-destructive`}
+                          //   onClick={() => deleteFn([transaction.id])}
+                        >
+                          Delete
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default TransactionTable;
