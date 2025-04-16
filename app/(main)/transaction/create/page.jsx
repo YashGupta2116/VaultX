81d1045145571fb9d75cbd3cca3798f7ec542763
Yash Gupta <yashgupta2116@gmail.com>
2025-04-16 23:10:36 +0530

    fixes

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,7 +1,8 @@
 import {getUserAccounts} from '@/actions/dashboard';
 import {defaultCategories} from '@/data/categories';
-import {AddTransactionForm} from '../_components/transaction-form';
+
 import {getTransaction} from '@/actions/transaction';
+import {AddTransactionForm} from '../_components/add-transaciton-form';
 
 export default async function AddTransactionPage({searchParams}) {
   const accounts = await getUserAccounts();
