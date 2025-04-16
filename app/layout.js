0b4fcc7dc4eb9be8604d46e69b2876f3cf1a1134
Yash Gupta <yashgupta2116@gmail.com>
2025-04-16 22:03:40 +0530

    bug

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,35 @@
-import './globals.css';
 import {Inter} from 'next/font/google';
+import './globals.css';
 import Header from '@/components/header';
+import {ClerkProvider} from '@clerk/nextjs';
 import {Toaster} from 'sonner';
-import ClientLayout from './layout.client';
 
 const inter = Inter({subsets: ['latin']});
 
 export const metadata = {
-  title: 'Vault X',
-  description: 'One stop Finance platform',
+  title: 'Welth',
+  description: 'One stop Finance Platform',
 };
 
 export default function RootLayout({children}) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>
-        <ClientLayout>
-          <header>
-            <Header />
-          </header>
+    <ClerkProvider>
+      <html lang='en'>
+        <head>
+          <link rel='icon' href='/logo-sm.png' sizes='any' />
+        </head>
+        <body className={`${inter.className}`}>
+          <Header />
           <main className='min-h-screen'>{children}</main>
           <Toaster richColors />
-          <footer className='bg-blue-100 py-12'>
+
+          <footer className='bg-blue-50 py-12'>
             <div className='container mx-auto px-4 text-center text-gray-600'>
-              © Made with by yash💖
+              <p>Made with 💗 by Yash</p>
             </div>
           </footer>
-        </ClientLayout>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
